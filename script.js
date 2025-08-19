f5846f2d4c04547aac5b67d1e110a8cfbbd2f162
Utemora <108686307+Utemora@users.noreply.github.com>
2025-08-19 20:02:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Кнопка-бургер
+const burger = document.querySelector(".burger");
+burger.addEventListener("click", () => {
+  alert("Открыть боковое меню (позже сделаем анимацию)");
+});
+
+// Кнопка корзины
+const cart = document.querySelector(".cart");
+cart.addEventListener("click", () => {
+  alert("Открыть корзину");
+});
+
+// Поиск
+const search = document.querySelector(".nav-right button:first-child");
+search.addEventListener("click", () => {
+  alert("Открыть поиск");
+});
 
