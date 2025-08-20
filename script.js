b1fbe54b7821594e12a5557d27d8db6ca9792c19
Utemora <108686307+Utemora@users.noreply.github.com>
2025-08-20 14:29:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function Button() {
+  console.log("Button works!");
+}
+
+window.Button = Button;
+
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot@1.1.2";
 import { cva, type VariantProps } from "class-variance-authority@0.7.1";
